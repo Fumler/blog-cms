3d73de14539d5afa0892bfc2594242fd147a9914
Fredrik Pettersen <fredrik.fumler@gmail.com>
2013-02-06 17:49:42 +0100

    Added twitter bootstrap

--- index.php
+++ index.php
@@ -2,17 +2,19 @@
 
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-	"http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="description" content="Open source blogging system (CMS)">
         <meta name="keywords" content="cms blog content managment system blogging">
         <title>Untitled</title>
-        <link rel="stylesheet" type="text/css" href="style/dark.css" media="all">
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link rel="stylesheet" type="text/css" href="css/dark.css" media="all">
     </head>
     <body>
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 
     </body>
 </html>
