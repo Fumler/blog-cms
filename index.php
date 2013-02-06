faca62f64df8dc56a55f9763a763e3b76fd2a3da
Fredrik Pettersen <fredrik.fumler@gmail.com>
2013-02-06 18:04:59 +0100

    Added some content

--- index.php
+++ index.php
@@ -8,11 +8,37 @@
         <meta charset="utf-8">
         <meta name="description" content="Open source blogging system (CMS)">
         <meta name="keywords" content="cms blog content managment system blogging">
-        <title>Untitled</title>
+        <title>Blog CMS</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link rel="stylesheet" type="text/css" href="css/dark.css" media="all">
     </head>
     <body>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#">Bloggyderp</a>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#about">Blogs</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    <div class="container">
+
+      <h1>Hello world!/h1>
+      <p>I said hello.</p>
+
+    </div>
         <script src="http://code.jquery.com/jquery-latest.js"></script>
         <script src="js/bootstrap.min.js"></script>
 
