2b1062e7136d3f9cff6d86d8263e4ccb7b888e77
Fredrik Pettersen <fredrik.fumler@gmail.com>
2013-02-06 17:35:44 +0100

    Added basic html setup, added new css files

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
-<!DOCTYPE html>
+<?php
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
 <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Open source blogging system (CMS)">
+        <meta name="keywords" content="cms blog content managment system blogging">
+        <title>Untitled</title>
+        <link rel="stylesheet" type="text/css" href="style/dark.css" media="all">
+    </head>
     <body>
-        20:11
+
     </body>
 </html>
