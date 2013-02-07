651ea218237b2377768457820364b6e7871ee19c
Fredrik Pettersen <fredrik.fumler@gmail.com>
2013-02-07 18:24:05 +0100

    Editing home page

--- pages/home.php
+++ pages/home.php
@@ -1,4 +1,11 @@
-<?php $page = "home";?>
-<h1>Hello world!</h1>
-<p>I said hello.</p>
-<p>This is home</p>
+<div class="row">
+  <div class="span3">
+  	<img src="http://lavileo.files.wordpress.com/2011/02/microphone-colored.jpg" alt="">
+  </div>
+  <div class="span6">
+  	<h1>What is a blog?</h1>
+  	<p>A blog can be your personal diary, or it can be a memo to the world. You can discuss many things in many forms and reach out to many different people.</p>
+  	<p>The blog is what you want it to be. There are millions of blogs in different colors, shapes and sizes and there are no rules.</p>
+  	<p>In short, a blog is a website where you can write whatever you want, and where the newest material is shown first. So that the viewers always have the latest news. And the users can comment on your posts to create a debate about different topics.</p>
+  </div>
+</div>
