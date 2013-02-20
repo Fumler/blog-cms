e988c37b0bd90e5d605cc445eafe9bee387286bf
Fredrik Pettersen <fredrik.fumler@gmail.com>
2013-02-20 22:02:40 +0100

    Added a license

--- pages/about.php
+++ pages/about.php
@@ -1,9 +1,27 @@
 <?php $page = "about";?>
 
-<h1>Login form</h1>
-<?php
+<h1>About us</h1>
+<p>This is a school project for the web technology course at Gjøvik University College. It is created by:</p>
 
-$passwordderp = "1"."test".SALT;
-$passwordsalted = hash_hmac('sha512', $passwordderp, SITEKEY);
-echo $passwordsalted;
-?>
+<ul>
+    <li>Inge Dalby</li>
+    <li>Fredrik Pettersen</li>
+    <li>Chritian Holt</li>
+    <li>Øyvind Nordli</li>
+</ul>
+
+<h1>License</h1>
+<p> Blog - A blog cms</p>
+<p>Copyright (C) 2013 Fredrik Pettersen, Inge Dalby, Christian Holt & Øyvind Nordli</p>
+<p>This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version. </p>
+
+<p>This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.</p>
+
+<p>You should have received a copy of the GNU General Public License
+along with this program.  If not, see <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
