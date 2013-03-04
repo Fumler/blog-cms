e1a32ca1843cabda94bd0577ac079ce7409779c2
Inge Dalby <inge.dalby@gmail.com>
2013-03-04 18:28:04 +0100

    furthering profile page

--- pages/profile.php
+++ pages/profile.php
@@ -1,13 +1,21 @@
 <h1><?php 
-	$user->getName() 
+	$user->getName();
 ?></h2>
 
-<h1>Profile - <?php echo $user->getName() ?></h1>
+<div class="row">
+	<div class="span8">
+		<h1>Profile - <?php echo $user->getName() ?></h1>
+	</div>
+	<div class="span2">
+		<button class="btn btn-warning" type="button">Edit Profile</button>
+	</div>
+</div>
+
 <dl class="dl-horizontal">
 	<dt>First name: </dt>
 
 	<dt>Last name: </dt>
-
+	
 	<dt>Email: </dt>
 
 	<dt>Address: </dt>
