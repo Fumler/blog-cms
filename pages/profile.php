323bb55de831d554c43f6b4d142396ea5d6ec88c
Inge Dalby <inge.dalby@gmail.com>
2013-03-04 18:06:12 +0100

    started on profile page

--- pages/profile.php
+++ pages/profile.php
@@ -1,1 +1,16 @@
-<h1><?php $user->getName() ?></h2>
+<h1><?php 
+	$user->getName() 
+?></h2>
+
+<h1>Profile - <?php echo $user->getName() ?></h1>
+<dl class="dl-horizontal">
+	<dt>First name: </dt>
+
+	<dt>Last name: </dt>
+
+	<dt>Email: </dt>
+
+	<dt>Address: </dt>
+
+	<dt>Info: </dt>
+</dl>
