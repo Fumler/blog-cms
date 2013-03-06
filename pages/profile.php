42cbf81ea0c9b8aab8bb0884131c73fd267e8092
Inge Dalby <inge.dalby@gmail.com>
2013-03-06 15:48:33 +0100

    trying uploading of images

--- pages/profile.php
+++ pages/profile.php
@@ -4,6 +4,82 @@
 
 <?php
   $uid = $_GET['prid'];
+  $posts = getPosts($uid);
+  $userInfo = getUser($uid);
+
+  define ("MAX_SIZE", "250");
+
+  // Check file extenstion
+  function getExtension($str) {
+    $i = strrpos($str, ".");
+    if (!$i) {
+      return "";
+    }
+
+    $l = strlen($str) - $i;
+
+    $ext = substr($str, $i+1, $l);
+    return $ext;
+  }
+
+  $errors = 0;
+
+  if (isset($_POST['Submit'])) {
+    $image = $_FILES['image']['name'];
+
+    if ($image) {
+      $filename = stripslashes($_FILES['image']['name']);
+      $extension = getExtension($filename);
+      $extension = strtolower($extension);
+
+      if (($extension != "jpg") && ($extension != "jpeg") && ($extension != "png") && ($extension != "gif")) {
+        ?>
+        <div class="alert alert-error">
+          <button type="button" class="close" data-dismiss="alert">&times;</button>
+          <?php echo "<p><strong>Invalid file extension</strong></p>" ?>
+        </div>
+        <?php 
+        $errors = 1;
+      }
+      else {
+        $size = filesize($_FILES['image']['tmp_name']);
+
+        if ($size > MAX_SIZE*1024) {
+          ?>
+          <div class="alert alert-error">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <?php echo "<p><strong>Filesize is too big!</strong></p>" ?>
+          </div> 
+          <?php
+          $errors = 1;
+        }
+
+        $image_name = $userInfo['uname'].time();
+        $newName = "img/".$image_name;
+
+        $copied = copy($_FILES['image']['tmp_name'], $newName);
+
+        if (!$copied) {
+          ?>
+          <div class="alert alert-error">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <?php echo "<p><strong>Copying of file unsuccessful. Try again!</strong></p>" ?>
+          </div> 
+          <?php
+          $errors = 1;
+        }
+      }
+    }
+  }
+
+  if (isset($_POST['Submit']) && !$errors) {
+    ?>
+    <div class="alert alert-error">
+      <button type="button" class="close" data-dismiss="alert">&times;</button>
+      <?php echo "<p><strong>Upload successful!</strong></p>" ?>
+    </div> 
+    <?php
+  }
 
   if (isset($_POST['oldPwd']) && strlen($_POST['oldPwd']) != 0) {
     if ((isset($_POST['newPwd']) && isset($_POST['newPwdA'])) && strlen($_POST['newPwd']) != 0) {
@@ -24,37 +100,42 @@
     $user->updateUser($uid, $_POST['fname'], $_POST['lname'], $_POST['email'], $_POST['address'], $_POST['info']);
   }
 
-	$posts = getPosts($uid);
-    $userInfo = getUser($uid);
-    ?>
-    <form method="post" action=<?php echo "index.php?id=profile" . "&prid=$uid" ?>>
-      <label>Profile Picture</label>
-      <img src=<?php echo $userInfo['pic']; ?>>
-      <label>Username</label>
-      <input type="text" value="<?php echo $userInfo['uname']; ?>" class="input-xlarge" disabled>
-      <label>First Name</label>
-      <input name="fname" type="text" value="<?php echo $userInfo['fname']; ?>" class="input-xlarge">
-      <label>Last Name</label>
-      <input name="lname" type="text" value="<?php echo $userInfo['lname']; ?>" class="input-xlarge">
-      <label>Email</label>
-      <input name="email" type="text" value="<?php echo $userInfo['email']; ?>" class="input-xlarge">
-      <label>Address</label>
-      <input name="address" type="text" value="<?php echo $userInfo['address']; ?>" class="input-xlarge">
-      <label>Info</label>
-      <input name="info" type="text" value="<?php echo $userInfo['info']; ?>" class="input-xlarge">
+  ?>
+  <form method="post" action=<?php echo "index.php?id=profile" . "&prid=$uid" ?>>
+    <label>Picture</label>
+    <img src=<?php echo $userInfo['pic']; ?>>
+    <div>
+      <form name="newad" method="post" enctype="multpart/form-data" action="">
+        <input name="image" type="file">
+        <input name="Submit" type="submit" value="Upload new image">
+      </form>
+    </div>
 
-      <legend>Password</legend>
-      <label>Old password</label>
-      <input name="oldPwd" type="password" class="input-xlarge">
-      <label>New password</label>
-      <input name="newPwd" type="password" class="input-xlarge">
-      <label>Again</label>
-      <input name="newPwdA" type="password" class="input-xlarge">
+    <label>Username</label>
+    <input type="text" value="<?php echo $userInfo['uname']; ?>" class="input-xlarge" disabled>
+    <label>First Name</label>
+    <input name="fname" type="text" value="<?php echo $userInfo['fname']; ?>" class="input-xlarge">
+    <label>Last Name</label>
+    <input name="lname" type="text" value="<?php echo $userInfo['lname']; ?>" class="input-xlarge">
+    <label>Email</label>
+    <input name="email" type="text" value="<?php echo $userInfo['email']; ?>" class="input-xlarge">
+    <label>Address</label>
+    <input name="address" type="text" value="<?php echo $userInfo['address']; ?>" class="input-xlarge">
+    <label>Info</label>
+    <input name="info" type="text" value="<?php echo $userInfo['info']; ?>" class="input-xlarge">
 
-      <div>
-        <button class="btn btn-primary" type="submit">Update</button>
-      </div>
-    </form>
+    <legend>Password</legend>
+    <label>Old password</label>
+    <input name="oldPwd" type="password" class="input-xlarge">
+    <label>New password</label>
+    <input name="newPwd" type="password" class="input-xlarge">
+    <label>Again</label>
+    <input name="newPwdA" type="password" class="input-xlarge">
+
+    <div>
+      <button class="btn btn-primary" type="submit">Update</button>
+    </div>
+  </form>
 <?php
 if($user->checkAdmin()) { ?>
     <div class="row">
