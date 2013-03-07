f8ccbbc09ef3de7eaf13634369699c4b82c55ca5
Garlov <oe.nordli@gmail.com>
2013-03-07 18:06:48 +0100

    can now edit posts

--- pages/viewpost.php
+++ pages/viewpost.php
@@ -61,14 +61,15 @@
           <p></p>
           <p>
             <i class="icon-calendar"></i>Posted <?php echo $post['created'];?>
-             - <i class="icon-user"></i>Written by <?php echo $userInfo['uname'];?>
+             | <i class="icon-user"></i>Written by <?php echo $userInfo['uname'];?>
              <?php if($owner)
              {
-                echo "- <i class='icon-trash'></i><a href='?removepost=$pid'>Remove post</a>";
+                echo "| <i class='icon-pencil'></i><a href='?id=editpost&pid=$pid'>Edit post</a>";
+                echo " | <i class='icon-trash'></i><a href='?removepost=$pid'>Remove post</a>";
              }
              else
              {
-              echo "- <i class='icon-exclamation-sign'></i><a href='?reportpost=$pid'>Report post as innapropriate</a>";
+              echo " | <i class='icon-exclamation-sign'></i><a href='?reportpost=$pid'>Report post as innapropriate</a>";
              }
              ?>
           </p>
